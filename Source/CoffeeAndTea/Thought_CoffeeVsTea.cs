882646169b2805329f51de8d0408bba80acf3987
Syrchalis <syrchalis@gmx.de>
2020-05-07 13:28:20 +0200

    Synchronization

---
+++ Source/CoffeeAndTea/Thought_CoffeeVsTea.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using System.Reflection;
+
+namespace CoffeeAndTea
+{
+    public class ThoughtWorker_CoffeeVsTea : ThoughtWorker
+    {
+        protected override ThoughtState CurrentSocialStateInternal(Pawn p, Pawn other)
+        {
+            if (!p.RaceProps.Humanlike)
+            {
+                return ThoughtState.Inactive;
+            }
+            if (!other.RaceProps.Humanlike)
+            {
+                return ThoughtState.Inactive;
+            }
+            if (!RelationsUtility.PawnsKnowEachOther(p, other))
+            {
+                return ThoughtState.Inactive;
+            }
+            float valuePawn = Rand.ValueSeeded(p.thingIDNumber ^ 125);
+            float valueOtherPawn = Rand.ValueSeeded(other.thingIDNumber ^ 125);
+            if (valuePawn < 0.2f || valueOtherPawn < 0.2f)
+            {
+                return ThoughtState.Inactive;
+            }
+            if ((valuePawn >= 0.6f && valueOtherPawn < 0.6f) || (valuePawn < 0.6f && valueOtherPawn >= 0.6f))
+            {
+                return ThoughtState.ActiveAtStage(0);
+            }
+            if (valuePawn < 0.6f && valueOtherPawn < 0.6f)
+            {
+                return ThoughtState.ActiveAtStage(1);
+            }
+            if (valuePawn >= 0.6f && valueOtherPawn >= 0.6f)
+            {
+                return ThoughtState.ActiveAtStage(2);
+            }
+            return ThoughtState.Inactive;
+        }
+    }
+}
---
+++ Source/CoffeeAndTea/ThougtWorker_CoffeeTea.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using System.Reflection;
+
+namespace CoffeeAndTea
+{
+    public class ThoughtWorker_Coffee : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            Hediff hediff = p.health.hediffSet.GetFirstHediffOfDef(CoffeeAndTeaDefOf.SyrCoffeeHigh);
+            if (hediff == null || hediff.def.stages == null)
+            {
+                return ThoughtState.Inactive;
+            }
+            float value = Rand.ValueSeeded(p.thingIDNumber ^ 125);
+            if (value >= 0.6f)
+            {
+                return ThoughtState.ActiveAtStage(1);
+            }
+            return ThoughtState.ActiveDefault;
+        }
+    }
+
+    public class ThoughtWorker_Tea : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            Hediff hediff = p.health.hediffSet.GetFirstHediffOfDef(CoffeeAndTeaDefOf.SyrTeaHigh);
+            if (hediff == null || hediff.def.stages == null)
+            {
+                return ThoughtState.Inactive;
+            }
+            float value = Rand.ValueSeeded(p.thingIDNumber ^ 125);
+            if (value >= 0.2 && value < 0.6f)
+            {
+                return ThoughtState.ActiveAtStage(1);
+            }
+            return ThoughtState.ActiveDefault;
+        }
+    }
+}
